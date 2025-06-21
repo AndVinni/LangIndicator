4960e4d4fa0ff8b33677052ee5e3027f0aee4740
AndVinni <andvinni@yandex.ru>
2025-06-21 17:28:55 +0300

    Uninstallation with termination of the running copy

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // Utility for displaying the keyboard layout at the place where you enter text.
-// V2.3
+// V2.4
 // Windows 11, C++ 20
 // (C) Vinni, June 2025
 
@@ -9,6 +9,7 @@
 #include "config.h"
 #include "langindicator.h"
 #include <Windows.h>
+#include <tlhelp32.h>
 
 HANDLE hThis = NULL;
 const wchar_t* szwMutex = L"93509C5Ф41164AД5АЕ70791F1В898А03";
@@ -16,7 +17,41 @@ const wchar_t* szwMutex = L"93509C5Ф41164AД5АЕ70791F1В898А03";
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR lpCmdLine, int)
 {
-    
+    // If an uninstall key is provided, terminate other copies of the program
+    if (lpCmdLine && wcsstr(lpCmdLine, L"-u"))
+    {
+        // Iterate through processes using Toolhelp to find other instances
+        HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (hSnap != INVALID_HANDLE_VALUE)
+        {
+            PROCESSENTRY32W pe = { sizeof(pe) };
+            if (Process32FirstW(hSnap, &pe))
+            {
+                wchar_t thisExe[MAX_PATH];
+                GetModuleFileNameW(nullptr, thisExe, MAX_PATH);
+                wchar_t exeName[MAX_PATH];
+                wchar_t* pName = wcsrchr(thisExe, L'\\');
+                wcscpy_s(exeName, MAX_PATH, pName ? pName + 1 : thisExe);
+                do
+                {
+                    if ((_wcsicmp(pe.szExeFile, exeName) == 0) && (pe.th32ProcessID != GetCurrentProcessId()))
+                    {
+                        HANDLE hProc = OpenProcess(PROCESS_TERMINATE, FALSE, pe.th32ProcessID);
+                        if (hProc)
+                        {
+                            TerminateProcess(hProc, 0);
+                            CloseHandle(hProc);
+                        }
+                    }
+                } while (Process32NextW(hSnap, &pe));
+            }
+            CloseHandle(hSnap);
+        }
+        // Remove from startup
+        UnregisterAutoRun();
+        return 0;
+    }
+
     hThis = GetCurrentProcess(); // This one
     HANDLE mutex = CreateMutexExW(0, szwMutex, CREATE_MUTEX_INITIAL_OWNER, READ_CONTROL);
     if (GetLastError() == ERROR_ALREADY_EXISTS)
@@ -25,12 +60,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR lpCmdLine, int)
         return 1;
     }
 
-    // Checking the "-u" key to remove from startup
-    if (lpCmdLine && wcsstr(lpCmdLine, L"-u"))
-    {
-        UnregisterAutoRun();
-        return 0;
-    }
     RegisterAutoRun();                    // autostart
 
     Config cfg;                           // configuration
