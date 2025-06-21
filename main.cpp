c5d94c100dad64a9069f9ed3cf59988f008e1c58
AndVinni <andvinni@yandex.ru>
2025-06-21 09:14:23 +0300

    Blocked the launch of the second copy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // Utility for displaying the keyboard layout at the place where you enter text.
 // V2.2
-// Windows 11 C++ 20
+// Windows 11, C++ 20
+// (C) Vinni, June 2025
 
 
 // main.cpp — entry point: autostart, load configuration, start indicator
@@ -9,9 +10,22 @@
 #include "langindicator.h"
 #include <Windows.h>
 
+HANDLE hThis = NULL;
+const wchar_t* szwMutex = L"93509C5Ф41164AД5АЕ70791F1В898А03";
+
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR lpCmdLine, int)
 {
+    
+    hThis = GetCurrentProcess(); // This one
+    HANDLE mutex = CreateMutexExW(0, szwMutex, CREATE_MUTEX_INITIAL_OWNER, READ_CONTROL);
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        MessageBoxExW(NULL, L"Application alredy run", L"Warning!", MB_OK, 0);
+        return 1;
+    }
+
+
     // Checking the "-u" key to remove from startup
     if (lpCmdLine && wcsstr(lpCmdLine, L"-u"))
     {
